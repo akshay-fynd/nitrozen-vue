088e91aa16ede6db7a5854d605c73818ffb7731e
Claudia Alphonse <claudiaalphonse@fynd-external.com>
2023-03-09 11:19:30 +0530

    added tooltip component changes and css file

--- src/components/NTooltip/NTooltip.vue
+++ src/components/NTooltip/NTooltip.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="nitrozen-tooltip">
     <nitrozen-inline :icon="icon"></nitrozen-inline>
-    <span class="nitrozen-tooltiptext" v-bind:class="tooltipPositionClass">
-      <template v-if="tooltipText">{{tooltipText}}</template>
+    <span class="nitrozen-tooltiptext" :class="tooltipPositionClass" :style="tooltipStyle">
+      <template v-if="tooltipText" >
+        <span>{{tooltipText}}</span>
+        </template>
+        <template v-if="link" >
+        <br/>
+        <a :href="link" class="nitrozen-tooltip-link">{{ tooltipLinkText }}</a>
+      </template>
       <template v-else><slot/></template>
     </span>
   </div>
@@ -17,6 +23,14 @@ export default {
     "nitrozen-inline": NitrozenInline
   },
   props: {
+    contentBgColor:{
+      type: String,
+      default: "#3d3d3d"
+    },
+    contentColor:{
+      type: String,
+      default: "#ffffff"
+    },
     position: {
       type: String,
       default: "bottom"
@@ -25,127 +39,33 @@ export default {
       type: String,
       default: ""
     },
-    icon: {
+    link: {
       type: String,
-      default: "tooltip"
+      default: ""
+    },
+    tooltipLinkText: {
+      type: String,
+      default: ""
+    },
+    icon: {
+      type: Element,
     }
   },
   computed: {
     tooltipPositionClass: function() {
+      return `nitrozen-tooltip-${this.position}`;
+    },
+    tooltipStyle: function () {
       return {
-        "nitrozen-tooltip-top": this.position == "top",
-        "nitrozen-tooltip-right": this.position == "right",
-        "nitrozen-tooltip-left": this.position == "left",
-        "nitrozen-tooltip-bottom": this.position == "bottom"
-      };
+        'background': this.contentBgColor,
+        'color': this.contentColor,
+        '--toolTipBgColor': this.contentBgColor
+      }
     }
   }
 };
 </script>
-
 <style lang="less">
 @import "./../../base/base.less";
-
-.nitrozen-tooltip {
-  position: relative;
-  display: inline-block;
-  cursor: pointer;
-
-  .nitrozen-tooltiptext {
-    visibility: hidden;
-    min-width: 150px;
-    color: @WhiteColor;
-    text-align: center;
-    border-radius: 6px;
-    font-family: @PrimaryFont;
-    position: absolute;
-    font-size: @BaseFontSize;
-    background-color: @Gray1;
-    padding: 10px;
-    z-index: 1000; /* Make sure the tooltip is above other elements */
-    line-height: 19px;
-    font-weight: 400;
-    max-width: 250px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    opacity: 0; /* Initially hidden */
-    transition: opacity 0.3s ease-in-out; 
-
-    & a{
-      color: @WhiteColor;
-        text-decoration: underline;
-        margin-top: 4px;
-        font-size: 11px;
-        font-weight: 400;
-    }
-
-    &::after {
-      content: " ";
-      position: absolute;
-      border-style: solid;
-      border-width: 6px;
-      border-color: @Gray1 transparent transparent transparent;
-    }
-  }
-
-  &:hover {
-    .nitrozen-tooltiptext {
-      visibility: visible;
-      opacity: 1;
-    }
-  }
-}
-
-/* Position the nitrozen-tooltip */
-.nitrozen-tooltip-bottom {
-  top: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-  margin-top: 10px;
-  &::after {
-    bottom: 100%; /* At the top of the tooltip */
-    left: 50%;
-    margin-left: -5px;
-    transform: rotate(180deg);
-  }
-}
-
-.nitrozen-tooltip-top {
-  bottom: 100%;
-  left: 50%;
-  transform: translateX(-50%);
-  margin-bottom: 10px;
-  &::after {
-    top: 100%; /* At the bottom of the tooltip */
-    left: 50%;
-    transform: translateX(-50%) rotate(0deg)
-  }
-}
-
-.nitrozen-tooltip-left {
-  top: 50% ;
-  right: 105%;
-  transform: translateY(-50%);
-  margin-right: 10px; 
-  &::after {
-    top: 50%;
-    left: 100%; /* To the left of the tooltip */
-    transform: rotate(270deg);
-    transform: translateY(-50%) rotate(270deg); 
-  }
-}
-
-.nitrozen-tooltip-right {
-  top: 50%;
-  left: 100%;
-  transform: translateY(-50%);
-  margin-left: 10px; 
-  &::after {
-    top: 50%;
-    right: 100%; /* To the right of the tooltip */;
-    transform: translateY(-50%) rotate(90deg); /* Pointing right */
-  }
-}
+@import "./NTooltip.less";
 </style>
